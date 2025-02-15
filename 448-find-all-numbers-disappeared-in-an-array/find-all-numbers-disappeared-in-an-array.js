c36bc7362fbe99037051abcaf1fcd8591730ea65
Abhishek  Jha <91289928+Abhishek-Jha-9@users.noreply.github.com>
2025-02-15 12:11:29 +0530

    Time: 45 ms (9.27%) | Memory: 74.9 MB (8.28%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -3,16 +3,6 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-    const ans = []
-    const s =  new Set();
-    nums.forEach(i=>s.add(i));
-    //  iterate through the set and find missing number
-    for(let i=1;i<=nums.length;i++){
-        if(!s.has(i)){
-            ans.push(i)
-        }
-    }
-
-    return ans;
-    
+    const s =  new Set(nums);
+    return Array.from({length:nums.length},(_,i)=>i+1).filter(i=> !s.has(i));
 };
